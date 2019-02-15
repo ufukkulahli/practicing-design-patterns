4e3c77cd44e7c282e1d57a2261f0db1e63691ed9
ufukkulahli <ufukkulahli@gmail.com>
2019-02-15 23:55:01 +0300

    Add of unit test for residence visitor example.

---
+++ behavioral/object/visitor/residence-example/ResidenceUnitTests.cs
@@ -0,0 +1,41 @@
+﻿using Xunit;
+
+namespace practicing_design_patterns.behavioral.@object.visitor
+{
+  public class ResidenceUnitTests
+  {
+    [Fact]
+    public void MaidVisitsResidence()
+    {
+      // Arrange
+      IResidenceVisitor maid = new Maid();
+      IRoom bathroom = new Bathroom();
+      IRoom bedroom = new Bedroom();
+      IRoom kitchen = new Kitchen();
+      IRoom livingRoom = new LivingRoom();
+
+      // Act
+      bathroom.Accept(maid);
+      bedroom.Accept(maid);
+      kitchen.Accept(maid);
+      livingRoom.Accept(maid);
+    }
+
+    [Fact]
+    public void PlumberVisitsResidence()
+    {
+      // Arrange
+      IResidenceVisitor plumber = new Plumber();
+      IRoom bathroom = new Bathroom();
+      IRoom bedroom = new Bedroom();
+      IRoom kitchen = new Kitchen();
+      IRoom livingRoom = new LivingRoom();
+
+      // Act
+      bathroom.Accept(plumber);
+      bedroom.Accept(plumber);
+      kitchen.Accept(plumber);
+      livingRoom.Accept(plumber);
+    }
+  }
+}
