6a7852e52a96d3b7c32fb94386099b1c1650ef80
ufukkulahli <ufukkulahli@gmail.com>
2019-04-29 23:55:58 +0300

    Add of new example named 'coffee' to Decorator patterns.

---
+++ structural/object/decorator/coffee/FilterCoffee.cs
@@ -0,0 +1,8 @@
+namespace practicing_design_patterns.structural.@object.decorator.coffee
+{
+  class FilterCoffee : ICoffee
+  {
+    public int Cost()=>10;
+    public string Description()=>"Filter coffee";
+  }
+}
---
+++ structural/object/decorator/coffee/ICoffee.cs
@@ -0,0 +1,8 @@
+namespace practicing_design_patterns.structural.@object.decorator.coffee
+{
+  interface ICoffee
+  {
+     int Cost();
+     string Description();
+  }
+}
