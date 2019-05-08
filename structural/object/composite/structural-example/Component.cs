05b0eca93dd19c207263fb34d91fbb5c9eb35638
Ufuk Kulahli <ufukkulahli@gmail.com>
2019-05-08 23:56:51 +0300

    Continue of Composite pattern with a structural example.

---
+++ structural/object/composite/structural-example/Component.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace practicing_design_patterns.structural.@object.composite.structural_example
+{
+  abstract class Component
+  {
+    protected string name;
+    internal Component(string name)=>this.name=name;
+    internal abstract void Add(Component component);
+    internal abstract void Remove(Component component);
+    internal abstract void Display(int depth);
+  }
+}
