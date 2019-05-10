7b925c6df007d3d62a42c364843a85fc93f3d877
ufukkulahli <ufukkulahli@gmail.com>
2019-05-10 23:30:19 +0300

    Beginning of new example named 'order' for Composite pattern.

---
+++ structural/object/composite/order/OrderUnitTests.cs
@@ -0,0 +1,17 @@
+using Xunit;
+
+namespace practicing_design_patterns.structural.@object.composite.order
+{
+  public class OrderUnitTests
+  {
+    [Fact]
+    public void Test()
+    {
+      // ARRANGE
+
+      // ACT
+
+      // ASSERT
+    }
+  }
+}
