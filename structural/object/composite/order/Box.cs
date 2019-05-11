ca8cb5a0be36bd23cf2ce54a0b3fa099ee2d5173
ufukkulahli <ufukkulahli@gmail.com>
2019-05-12 00:44:08 +0300

    Complete of 'order' example for Composite pattern.

--- structural/object/composite/order/Box.cs
+++ structural/object/composite/order/Box.cs
@@ -6,9 +6,16 @@ namespace practicing_design_patterns.structural.@object.composite.order
   public sealed class Box : IProduct
   {
     private readonly decimal price;
+    private readonly string name;
     private readonly ICollection<IProduct> products = new List<IProduct>();
-    public Box() : this(0) {}
-    public Box(decimal price) => this.price = price;
+    public Box() : this(0, "") {}
+    public Box(string name) : this(0, name) {}
+    public Box(decimal price) : this(price, "") {}
+    public Box(decimal price, string name)
+    {
+      this.price = price;
+      this.name = name;
+    }
     public void AddProduct(IProduct product) => this.products.Add(product);
     public void RemoveProduct(IProduct product) => this.products.Remove(product);
     public decimal Price()
--- structural/object/composite/order/OrderUnitTests.cs
+++ structural/object/composite/order/OrderUnitTests.cs
@@ -8,17 +8,17 @@ namespace practicing_design_patterns.structural.@object.composite.order
     public void AssertTotalPriceTrue()
     {
       // ARRANGE
-      var smalliPhoneBox = new Box();
+      var smalliPhoneBox = new Box("smalliPhoneBox");
       smalliPhoneBox.AddProduct(new Product(500, "iPhone7"));
 
-      var smallPixelBox = new Box();
+      var smallPixelBox = new Box("smallPixelBox");
       smallPixelBox.AddProduct(new Product(500, "Pixel"));
 
-      var midBox = new Box(3);
+      var midBox = new Box(3, "midBox");
       midBox.AddProduct(smalliPhoneBox);
       midBox.AddProduct(smallPixelBox);
 
-      var bigBox = new Box(5);
+      var bigBox = new Box(5, "bigBox");
       bigBox.AddProduct(midBox);
       bigBox.AddProduct(new Product(100, "Printer"));
 
