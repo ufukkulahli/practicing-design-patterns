36b6cdf8470d83db30529570da943a5bec793a8b
ufukkulahli <ufukkulahli@gmail.com>
2019-05-20 23:15:06 +0300

    Continue of new example for Composite pattern.

---
+++ structural/object/composite/ sentence-example/Messenger.cs
@@ -0,0 +1,11 @@
+namespace practicing_design_patterns.structural.@object.composite.sentence_example
+{
+  public sealed class Messenger
+  {
+    public LetterComposite MessageFromOrcs()
+    {
+      //todo
+      return null;
+    }
+  }
+}
