78f4158f02ad1ea74bd00210736416a061b7c759
ufukkulahli <ufukkulahli@gmail.com>
2019-06-10 23:13:06 +0300

    Added test for Interpreter pattern example.

--- behavioral/class/interpreter/expression/ExpressionUnitTests.cs
+++ behavioral/class/interpreter/expression/ExpressionUnitTests.cs
@@ -5,7 +5,7 @@ namespace practicing_design_patterns.behavioral.@class.interpreter.expression
   public class ExpressionUnitTests
   {
     [Fact]
-    public void Test()
+    public void AddsTwoNumbers()
     {
       // Arrange
       var adding = new AddExpression(new ConstantExpression(1), new ConstantExpression(1));
@@ -15,6 +15,21 @@ namespace practicing_design_patterns.behavioral.@class.interpreter.expression
 
       // Assert
       Assert.Equal(2, result);
+    }
+    
+    [Fact]
+    public void DoesLittleBitMoreComplicatedCalculation()
+    {
+      // Arrange
+      var adding = new AddExpression(new ConstantExpression(1), new ConstantExpression(1));
+      var subtracting = new SubtractExpression(new ConstantExpression(4), new ConstantExpression(2));
+      var subtractingResult = new SubtractExpression(adding, subtracting);
+
+      // Act
+      var result = subtractingResult.Solve(null);
+
+      // Assert
+      Assert.Equal(0, result);
     }
   }
 }
