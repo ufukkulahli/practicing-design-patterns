2aa693086c4006122481e027e7e0c2191b045192
ufukkulahli <ufukkulahli@gmail.com>
2019-09-24 23:07:13 +0300

    Start an example for 'factory-method' named 'transportation'.

---
+++ creational/class/factory-method/transportation/Ship.cs
@@ -0,0 +1,10 @@
+namespace practicing_design_patterns.creational.@class.factory_method.transportation
+{
+  public sealed class Ship : Transport
+  {
+    public void Deliver()
+    {
+      throw new System.NotImplementedException();
+    }
+  }
+}
---
+++ creational/class/factory-method/transportation/Transport.cs
@@ -0,0 +1,7 @@
+namespace practicing_design_patterns.creational.@class.factory_method.transportation
+{
+  public interface Transport
+  {
+    void Deliver();
+  }
+}
