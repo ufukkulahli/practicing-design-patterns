393682f8d70d6aed03622902542cc2905b0e8401
ufukkulahli <ufukkulahli@gmail.com>
2019-09-26 23:28:16 +0300

    Start a new example named 'dessert' for 'factory-method' pattern.

---
+++ creational/class/factory-method/dessert/DessertUnitTests.cs
@@ -0,0 +1,18 @@
+using Xunit;
+
+namespace practicing_design_patterns.creational.@class.factory_method.dessert
+{
+  public class DessertUnitTests
+  {
+    [Fact]
+    public void Test()
+    {
+      // ARRANGE
+
+      // ACT
+
+      // ASSERT
+
+    }
+  }
+}
