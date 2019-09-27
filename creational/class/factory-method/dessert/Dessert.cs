ff65f82682518c3d6d067091daecd5183d40087e
ufukkulahli <ufukkulahli@gmail.com>
2019-09-27 22:00:27 +0300

    Continue to 'dessert' example.

---
+++ creational/class/factory-method/dessert/Dessert.cs
@@ -0,0 +1,7 @@
+namespace practicing_design_patterns.creational.@class.factory_method.dessert
+{
+  public abstract class Dessert
+  {
+    public abstract string Serve();
+  }
+}
---
+++ creational/class/factory-method/dessert/Tiramisu.cs
@@ -0,0 +1,10 @@
+namespace practicing_design_patterns.creational.@class.factory_method.dessert
+{
+  public sealed class Tiramisu : Dessert
+  {
+    public override string Serve()
+    {
+      return "Here is the Tiramisu";
+    }
+  }
+}
