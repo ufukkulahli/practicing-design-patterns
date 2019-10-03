688f2981540731f67e1611537b5a7ec916e19eae
ufukkulahli <ufukkulahli@gmail.com>
2019-10-03 23:13:23 +0300

    Start of 'button' example for 'abstract-factory' pattern.

---
+++ creational/object/abstract-factory/button/ButtonUnitTests.cs
@@ -0,0 +1,18 @@
+using Xunit;
+
+namespace practicing_design_patterns.creational.@object.abstract_factory.button
+{
+  public class ButtonUnitTests
+  {
+    [Fact]
+    public void Test()
+    {
+      // Arrange
+
+      // Act
+
+      // Assert
+
+    }
+  }
+}
