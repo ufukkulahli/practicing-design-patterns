e8e9022f9cbdc66125eaff231e7808aa1fa5d0f6
ufukkulahli <ufukkulahli@gmail.com>
2019-10-21 23:16:18 +0300

    Add new test for 'billing' example.

--- behavioral/object/strategy/billing/BillingUnitTests.cs
+++ behavioral/object/strategy/billing/BillingUnitTests.cs
@@ -52,5 +52,23 @@ namespace practicing_design_patterns.behavioral.@object.strategy.billing
       // Assert
       Assert.Equal(new decimal(7.5), amount);
     }
+
+    [Fact]
+    public void ApplyHappyHourBillingToCustomer()
+    {
+      // Arrange
+      var customer = new Customer();
+      Billing happyHour = new HappyHour();
+      Beverage beer1 = new Drink(new Money(new Dollar(5)), happyHour);
+      Beverage beer2 = new Drink(new Money(new Dollar(5)), happyHour);
+      customer.Consume(beer1);
+      customer.Consume(beer2);
+
+      // Act
+      var amount = customer.Pay();
+
+      // Assert
+      Assert.Equal(new decimal(5), amount);
+    }
   }
 }
