739afd4296701dde8a8427d6dd9c4abfae8e9d3d
ufukkulahli <ufukkulahli@gmail.com>
2019-11-10 22:33:19 +0300

    Start new example named 'text editor app' for 'command' pattern.

---
+++ behavioral/object/command/text-editor-app/TextEditorApp.cs
@@ -0,0 +1,6 @@
+namespace practicing_design_patterns.behavioral.@object.command.text_editor_app
+{
+  public sealed class TextEditorApp
+  {
+  }
+}
---
+++ behavioral/object/command/text-editor-app/TextEditorAppUnitTests.cs
@@ -0,0 +1,20 @@
+using Xunit;
+
+namespace practicing_design_patterns.behavioral.@object.command.text_editor_app
+{
+  public class TextEditorUnitTests
+  {
+    [Fact]
+    public void Test()
+    {
+      // Arrange
+      var textEditor = new TextEditorApp();
+      
+      // Act
+      // TODO
+
+      // Assert
+      Assert.True(true);
+    }
+  }
+}
