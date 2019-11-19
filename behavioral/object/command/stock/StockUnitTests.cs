992c8e65b3e649403b2862e3cfac82e93e2e3d03
ufukkulahli <ufukkulahli@gmail.com>
2019-11-19 22:24:10 +0300

    Start new example named buy&sell 'stock'.

---
+++ behavioral/object/command/stock/StockUnitTests.cs
@@ -0,0 +1,29 @@
+using Xunit;
+
+namespace practicing_design_patterns.behavioral.@object.command.stock
+{
+  public class StockUnitTests
+  {
+    [Fact]
+    public void BuysStock()
+    {
+      // TODO
+      // Arrange
+
+      // Act
+
+      // Assert
+    }
+
+    [Fact]
+    public void SellsStock()
+    {
+      // TODO
+      // Arrange
+
+      // Act
+
+      // Assert
+    }
+  }
+}
