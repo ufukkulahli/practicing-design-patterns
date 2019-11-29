df1a6df3d234ed6a96f91d2d71eb91ff1ec18d41
ufukkulahli <ufukkulahli@gmail.com>
2019-11-29 23:32:40 +0300

    Start of new example named 'job seeking' for 'Observer' pattern.

---
+++ behavioral/object/observer/job-seeking/JobSeekingUnitTests.cs
@@ -0,0 +1,16 @@
+namespace practicing_design_patterns.behavioral.@object.observer.job_seeking
+{
+  public class JobSeekingUnitTests
+  {
+    public void Test()
+    {
+      // TODO
+      // Arrange
+
+      // Act
+
+      // Assert
+
+    }
+  }
+}
