86e958cb309465441f1e9673dcfe268bb38d3a2f
ufukkulahli <ufukkulahli@gmail.com>
2019-12-02 23:05:58 +0300

    Start new example for 'Observer' pattern.

---
+++ behavioral/object/observer/stock-price-change/StockPriceChange.cs
@@ -0,0 +1,19 @@
+using Xunit;
+
+namespace practicing_design_patterns.behavioral.@object.observer.stock_price_change
+{
+  public class StockPriceChange
+  {
+    [Fact]
+    public void Test()
+    {
+      // TODO
+      // Arrange
+
+      // Act
+
+      // Assert
+
+    }
+  }
+}
