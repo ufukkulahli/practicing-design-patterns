9d9abf9fa197258bffe2ce0aaa17622cd52d3f17
ufukkulahli <ufukkulahli@gmail.com>
2019-12-05 23:35:52 +0300

    Start of new example named 'text editor notifies services' for 'Observer' pattern.

---
+++ behavioral/object/observer/text-editor-notifies-services/TextEditorNotifiesServices.cs
@@ -0,0 +1,19 @@
+using Xunit;
+
+namespace practicing_design_patterns.behavioral.@object.observer.editor_notifies_services
+{
+  public class TextEditorNotifiesServices
+  {
+    [Fact]
+    public void NotifyOtherServicesWhenAnEventOccurs()
+    {
+      // TODO
+      // Arrange
+
+      // Act
+
+      // Assert
+
+    }
+  }
+}
