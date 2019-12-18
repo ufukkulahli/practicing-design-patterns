79050b1bec6b01a7ea85da24d5fe09e25f6535e5
ufukkulahli <ufukkulahli@gmail.com>
2019-12-18 23:16:18 +0300

    Start of new example for 'Memento' pattern.

---
+++ behavioral/object/memento/employee/EmployeeUnitTests.cs
@@ -0,0 +1,15 @@
+namespace practicing_design_patterns.behavioral.@object.memento.employee
+{
+  public class EmployeeUnitTests
+  {
+    public void SavesAndRestoresEmployeesPersonalInfo()
+    {
+      // Arrange
+
+      // Act
+
+      // Assert
+
+    }
+  }
+}
