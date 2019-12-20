c47a5be7c3417a6846e5f24c9f7d570f322d7f4d
ufukkulahli <ufukkulahli@gmail.com>
2019-12-20 22:50:31 +0300

    Add new test for 'employee' example.

--- behavioral/object/memento/employee/EmployeeUnitTests.cs
+++ behavioral/object/memento/employee/EmployeeUnitTests.cs
@@ -26,5 +26,33 @@ namespace practicing_design_patterns.behavioral.@object.memento.employee
       // Assert
       Assert.Equal("5554443322", joe.Phone);
     }
+
+    [Fact]
+    public void SavesAndRestoresMultipleEmployeesPersonalInfo()
+    {
+      // Arrange
+      var joe = new Employee { Name = "Joe Dalton", Phone = "5554443322" };
+      var jack = new Employee { Name = "Jack Dalton", Phone = "9998887766" };
+
+      var employeeHistory = new EmployeeHistory();
+      employeeHistory.Save(joe);
+      employeeHistory.Save(jack);
+
+      // (Update Joe's phone)
+      joe.Phone = "1112223344";
+      Assert.Equal("1112223344", joe.Phone);
+
+      // (Update Jack's phone)
+      jack.Phone = "0009998877";
+      Assert.Equal("0009998877", jack.Phone);
+
+      // Act
+      employeeHistory.RevertChangesToPriorOf(joe);
+      employeeHistory.RevertChangesToPriorOf(jack);
+
+      // Assert
+      Assert.Equal("5554443322", joe.Phone);
+      Assert.Equal("9998887766", jack.Phone);
+    }
   }
 }
