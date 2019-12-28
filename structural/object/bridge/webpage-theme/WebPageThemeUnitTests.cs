aae48b6bc73fc1a9f3f3c6ce52bb76351bdc20ea
ufukkulahli <ufukkulahli@gmail.com>
2019-12-28 23:05:44 +0300

    Start of example 'web page theme' for 'Bridge' pattern.

---
+++ structural/object/bridge/webpage-theme/WebPageThemeUnitTests.cs
@@ -0,0 +1,18 @@
+using Xunit;
+
+namespace practicing_design_patterns.structural.@object.bridge.webpage_theme
+{
+  public class WebPageThemeUnitTests
+  {
+    [Fact]
+    public void ChangesWebPagesTheme()
+    {
+      // Arrange
+
+      // Act
+
+      // Assert
+
+    }
+  }
+}
