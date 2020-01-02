19d00030652871b94072970047b2d04a8cf9130b
ufukkulahli <ufukkulahli@gmail.com>
2020-01-02 22:33:17 +0300

    Complete minimal implementation of 'thread scheduler' example with a test.

--- structural/object/bridge/thread-scheduler/PreemtiveThreadScheduler.cs
+++ structural/object/bridge/thread-scheduler/PreemtiveThreadScheduler.cs
@@ -2,5 +2,8 @@ namespace practicing_design_patterns.structural.@object.bridge.thread_scheduler
 {
   public sealed class PreemtiveThreadScheduler : ThreadScheduler
   {
+    private OSSpecificThreadScheduler ossts;
+    public PreemtiveThreadScheduler(OSSpecificThreadScheduler ossts) => this.ossts = ossts;
+    public bool Start() => this.ossts.Start();
   }
 }
--- structural/object/bridge/thread-scheduler/ThreadSchedulerUnitTests.cs
+++ structural/object/bridge/thread-scheduler/ThreadSchedulerUnitTests.cs
@@ -8,12 +8,14 @@ namespace practicing_design_patterns.structural.@object.bridge.thread_scheduler
     public void WindowsThreadScheduler()
     {
       // Arrange
-      ThreadScheduler ts = new PreemtiveThreadScheduler();
+      OSSpecificThreadScheduler ossts = new WindowsTS();
+      ThreadScheduler ts = new PreemtiveThreadScheduler(ossts);
 
       // Act
+      var started = ts.Start();
 
       // Assert
-
+      Assert.True(started);
     }
   }
 }
