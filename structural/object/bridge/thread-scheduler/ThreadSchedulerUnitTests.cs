d6d9ee0c9a699e7cb6d9d0ead07803469dca25e8
ufukkulahli <ufukkulahli@gmail.com>
2020-01-05 21:45:41 +0300

    New unit test for 'thread scheduler' example.

--- structural/object/bridge/thread-scheduler/ThreadSchedulerUnitTests.cs
+++ structural/object/bridge/thread-scheduler/ThreadSchedulerUnitTests.cs
@@ -45,5 +45,19 @@ namespace practicing_design_patterns.structural.@object.bridge.thread_scheduler
       // Assert
       Assert.False(started);
     }
+
+    [Fact]
+    public void PreemtiveThreadSchedulerForLinuxImplementation()
+    {
+      // Arrange
+      OSSpecificThreadScheduler ossts = new LinuxTS();
+      ThreadScheduler ts = new PreemtiveThreadScheduler(ossts);
+
+      // Act
+      var started = ts.Start();
+
+      // Assert
+      Assert.False(started);
+    }
   }
 }
