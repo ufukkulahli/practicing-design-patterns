d530db660e16e1fed56844ea905f7d8e6464d3e1
ufukkulahli <ufukkulahli@gmail.com>
2020-01-08 23:16:34 +0300

    New example for 'Flyweight' pattern named 'company'.

---
+++ structural/object/flyweight/company/CompanyUnitTests.cs
@@ -0,0 +1,18 @@
+using Xunit;
+
+namespace practicing_design_patterns.structural.@object.flyweight.company
+{
+  public class CompanyUnitTests
+  {
+    [Fact]
+    public void Test()
+    {
+      // Arrange
+
+      // Act
+
+      // Assert
+
+    }
+  }
+}
