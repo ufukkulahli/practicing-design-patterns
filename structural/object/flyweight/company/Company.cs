32abdabb74a3f0bbddbdd69029258be389235428
ufukkulahli <ufukkulahli@gmail.com>
2020-01-08 23:27:11 +0300

    Implement bare minimum for 'company' example.

---
+++ structural/object/flyweight/company/Company.cs
@@ -0,0 +1,18 @@
+namespace practicing_design_patterns.structural.@object.flyweight.company
+{
+  public sealed class Company : ICompany
+  {
+    private string name = string.Empty;
+    private string location = string.Empty;
+    private string agreement = string.Empty;
+
+    public Company(string name, string location)
+    {
+      this.name = name;
+      this.location = location;
+    }
+
+    public string Name { get=>this.name; }
+    public string Agreement { get=>this.agreement; }
+  }
+}
--- structural/object/flyweight/company/CompanyUnitTests.cs
+++ structural/object/flyweight/company/CompanyUnitTests.cs
@@ -5,14 +5,16 @@ namespace practicing_design_patterns.structural.@object.flyweight.company
   public class CompanyUnitTests
   {
     [Fact]
-    public void Test()
+    public void VerifyCompanyInfo()
     {
       // Arrange
+      var orangeCompany = new Company("Orange", "USA");
 
-      // Act
-
-      // Assert
-
+      // Act && Assert
+      Assert.Equal("Orange", orangeCompany.Name);
+      //TODO
+      //Assert.Equal("A huge agreement file", orangeCompany.Agreement);
+      Assert.Equal("", orangeCompany.Agreement);
     }
   }
 }
