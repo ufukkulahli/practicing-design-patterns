0859b6fc6dfb9874bf88580400495d1f66f7fc63
ufukkulahli <ufukkulahli@gmail.com>
2020-01-10 23:20:28 +0300

    New example named 'tree' for 'flyweight' pattern.

---
+++ structural/object/flyweight/tree/TreeUnitTests.cs
@@ -0,0 +1,18 @@
+using Xunit;
+
+namespace practicing_design_patterns.structural.@object.flyweight.tree
+{
+  public class TreeUnitTests
+  {
+    [Fact]
+    public void CreatesLightweightTrees()
+    {
+      // Arrange
+
+      // Act
+
+      // Assert
+
+    }
+  }
+}
