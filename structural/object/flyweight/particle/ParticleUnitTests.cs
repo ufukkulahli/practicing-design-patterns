013da7f80548187e90f64fd93b2744672d8eebf5
ufukkulahli <ufukkulahli@gmail.com>
2020-01-13 23:21:02 +0300

    New example named 'particle'.

---
+++ structural/object/flyweight/particle/ParticleUnitTests.cs
@@ -0,0 +1,15 @@
+namespace practicing_design_patterns.structural.@object.flyweight.particle
+{
+  public class ParticleUnitTests
+  {
+    public void Test()
+    {
+      // Arrange
+
+      // Act
+
+      // Assert
+
+    }
+  }
+}
