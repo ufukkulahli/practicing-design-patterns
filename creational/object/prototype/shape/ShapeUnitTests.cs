77d7b703c53c390931fe9354f12aec3d69dfc44c
ufukkulahli <ufukkulahli@gmail.com>
2020-01-19 14:51:05 +0300

    New example for 'Prototype' pattern, named 'shape'.

---
+++ creational/object/prototype/shape/ShapeUnitTests.cs
@@ -0,0 +1,18 @@
+using Xunit;
+
+namespace practicing_design_patterns.creational.@object.prototype.shape
+{
+  public class ShapeUnitTests
+  {
+    [Fact]
+    public void Test()
+    {
+      // Arrange
+
+      // Act
+
+      // Assert
+      Assert.True(true);
+    }
+  }
+}
