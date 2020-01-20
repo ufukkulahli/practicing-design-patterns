9b885e2475627c66cf19ccd2aa9ff281cef8afea
ufukkulahli <ufukkulahli@gmail.com>
2020-01-20 22:41:49 +0300

    New test for 'shape' example.

--- creational/object/prototype/shape/ShapeUnitTests.cs
+++ creational/object/prototype/shape/ShapeUnitTests.cs
@@ -25,5 +25,30 @@ namespace practicing_design_patterns.creational.@object.prototype.shape
       Assert.Equal(5, brandNewCircle.Y);
       Assert.Equal(10, brandNewCircle.Radius);
     }
+
+    [Fact]
+    public void ClonesCircleAndUpdatesItsProperties()
+    {
+      // Arrange
+      var circle = new Circle();
+      circle.X = 5;
+      circle.Y = 5;
+      circle.Radius = 10;
+
+      // Act
+      var brandNewCircle = circle.Clone() as Circle;
+      brandNewCircle.X = 10;
+      brandNewCircle.Y = 10;
+      brandNewCircle.Radius = 20;
+
+      // Assert
+      Assert.Equal(5, circle.X);
+      Assert.Equal(5, circle.Y);
+      Assert.Equal(10, circle.Radius);
+
+      Assert.Equal(10, brandNewCircle.X);
+      Assert.Equal(10, brandNewCircle.Y);
+      Assert.Equal(20, brandNewCircle.Radius);
+    }
   }
 }
