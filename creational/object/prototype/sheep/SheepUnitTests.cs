b4b4bbcdb9cc83a4073ce3197848f0cb66305042
ufukkulahli <ufukkulahli@gmail.com>
2020-01-21 23:01:30 +0300

    New example named 'sheep' for 'Prototype' pattern.

---
+++ creational/object/prototype/sheep/SheepUnitTests.cs
@@ -0,0 +1,18 @@
+using Xunit;
+
+namespace practicing_design_patterns.creational.@object.prototype.sheep
+{
+  public class SheepUnitTests
+  {
+    [Fact]
+    public void ClonesSheep()
+    {
+      // Arrange
+
+      // Act
+
+      // Assert
+      Assert.True(true);
+    }
+  }
+}
