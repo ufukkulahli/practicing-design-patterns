3125d62ac3688e2b061904b7499996bdc3f14409
ufukkulahli <ufukkulahli@gmail.com>
2020-01-24 23:03:20 +0300

    New test named 'robot' for 'Prototype' pattern.

---
+++ creational/object/prototype/robot/RobotUnitTests.cs
@@ -0,0 +1,18 @@
+using Xunit;
+
+namespace practicing_design_patterns.creational.@object.prototype.robot
+{
+  public class RobotUnitTests
+  {
+    [Fact]
+    public void ClonesRobot()
+    {
+      // Arrange
+
+      // Act
+
+      // Assert
+      Assert.True(true);
+    }
+  }
+}
