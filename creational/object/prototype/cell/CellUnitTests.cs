564d411934dba3aaee52a02f9c28b910b55bb469
ufukkulahli <ufukkulahli@gmail.com>
2020-01-26 23:19:15 +0300

    New example named 'cell' for 'Prototype' pattern.

---
+++ creational/object/prototype/cell/CellUnitTests.cs
@@ -0,0 +1,18 @@
+using Xunit;
+
+namespace practicing_design_patterns.creational.@object.prototype.cell
+{
+  public class CellUnitTests
+  {
+    [Fact]
+    public void CellSplitsItself()
+    {
+      // Arrange
+
+      // Act
+
+      // Assert
+      Assert.True(true);
+    }
+  }
+}
