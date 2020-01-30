eb60691c632a0c6a731e14782267f20495455a29
ufukkulahli <ufukkulahli@gmail.com>
2020-01-30 22:31:29 +0300

    New example named 'house' for 'Builder' pattern.

---
+++ creational/object/builder/house/HouseBuilderUnitTests.cs
@@ -0,0 +1,18 @@
+using Xunit;
+
+namespace practicing_design_patterns.creational.@object.builder.house
+{
+  public class HouseBuilderUnitTests
+  {
+    [Fact]
+    public void BuildsHouse()
+    {
+      // Arrange
+
+      // Act
+
+      // Assert
+      Assert.True(true);
+    }
+  }
+}
