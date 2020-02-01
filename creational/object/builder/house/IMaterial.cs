12e7dcbfb6af25ef157d594db99c28c30329bf85
ufukkulahli <ufukkulahli@gmail.com>
2020-02-01 23:13:42 +0300

    Continue implementation of 'house builder' example.

--- creational/object/builder/house/IMaterial.cs
+++ creational/object/builder/house/IMaterial.cs
@@ -2,5 +2,6 @@ namespace practicing_design_patterns.creational.@object.builder.house
 {
   public interface IMaterial
   {
+    string Definition {get;}
   }
 }
--- creational/object/builder/house/Wood.cs
+++ creational/object/builder/house/Wood.cs
@@ -2,5 +2,6 @@ namespace practicing_design_patterns.creational.@object.builder.house
 {
   public sealed class Wood : IMaterial
   {
+    public string Definition => "Wooden material";
   }
 }
