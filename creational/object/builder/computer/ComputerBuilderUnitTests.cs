5709b2ca90bad4b7d0926abfc1da0aa2eec3a977
ufukkulahli <ufukkulahli@gmail.com>
2020-02-04 19:58:41 +0300

    New example named 'computer builder'.

---
+++ creational/object/builder/computer/ComputerBuilderUnitTests.cs
@@ -0,0 +1,18 @@
+using Xunit;
+
+namespace practicing_design_patterns.creational.@object.builder.computer
+{
+  public class ComputerBuilderUnitTests
+  {
+    [Fact]
+    public void BuildsCustomComputer()
+    {
+      // Arrange
+
+      // Act
+
+      // Assert
+      Assert.True(true);
+    }
+  }
+}
