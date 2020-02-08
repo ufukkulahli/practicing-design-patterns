616b03b2181576656e94d0a15144f9968d2c4c34
ufukkulahli <ufukkulahli@gmail.com>
2020-02-08 22:05:10 +0300

    Complete 'BuildsCustomComputerWithNonOEMParts' test.

--- creational/object/builder/computer/ComputerBuilderUnitTests.cs
+++ creational/object/builder/computer/ComputerBuilderUnitTests.cs
@@ -22,5 +22,28 @@ namespace practicing_design_patterns.creational.@object.builder.computer
       // Assert
       Assert.Equal("OEMMotherboard, 16GB RAM, 8GB graphics card, 1TB HDD, Intel i7", computer.HardwareInfo());
     }
+
+    [Fact]
+    public void BuildsCustomComputerWithNonOEMParts()
+    {
+      // Arrange
+      var computerBuilder = new ComputerBuilder();
+
+      // Act
+      var computer = computerBuilder
+                        .Motherboard(new MotherBoard("Gigabyte"))
+                        .Ram(new Ram("Kingstone 32GB"))
+                        .GraphicsCard(new GraphicsCard("Nvidia 16GB"))
+                        .HardDiskDrive(new HDD("Seagate 1TB"))
+                        .CPU(new CPU("Intel i7"))
+                        .Build();
+
+      // Assert
+      Assert.Equal
+      (
+        "Gigabyte, Kingstone 32GB RAM, Nvidia 16GB graphics card, Seagate 1TB HDD, Intel i7",
+        computer.HardwareInfo()
+      );
+    }
   }
 }
