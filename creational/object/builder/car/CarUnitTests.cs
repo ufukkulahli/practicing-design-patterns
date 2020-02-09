bb16a3ab11af58905c81c4b6402520f967fc0f5e
ufukkulahli <ufukkulahli@gmail.com>
2020-02-09 22:26:19 +0300

    New example named 'car builder'.

---
+++ creational/object/builder/car/CarUnitTests.cs
@@ -0,0 +1,18 @@
+using Xunit;
+
+namespace practicing_design_patterns.creational.@object.builder.car
+{
+  public class CarUnitTests
+  {
+    [Fact]
+    public void BuildsRegularCar()
+    {
+      // Arrange
+
+      // Act
+
+      // Assert
+      Assert.True(true);
+    }
+  }
+}
