821fe02096d9f0836d75c782433d096ec75bae85
ufukkulahli <ufukkulahli@gmail.com>
2020-02-12 22:30:34 +0300

    Continue to 'car builder' example.

--- creational/object/builder/car/PorscheBuilder.cs
+++ creational/object/builder/car/PorscheBuilder.cs
@@ -1,6 +1,10 @@
 namespace practicing_design_patterns.creational.@object.builder.car
 {
-  public sealed class PorscheBuilder
+  public sealed class PorscheBuilder : ICarBuilder
   {
+    private string color;
+    private int doors;
+    public void Color(string value) => this.color = value;
+    public void Doors(int value) => this.doors = value;
   }
 }
