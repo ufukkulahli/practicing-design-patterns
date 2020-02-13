b93ca7acf1d09ee553113265b00c680e51ee7157
ufukkulahli <ufukkulahli@gmail.com>
2020-02-13 23:19:40 +0300

    Complete 'car builder' example.

--- creational/object/builder/car/CarUnitTests.cs
+++ creational/object/builder/car/CarUnitTests.cs
@@ -40,5 +40,23 @@ namespace practicing_design_patterns.creational.@object.builder.car
       Assert.Equal("Silver", porsche.Color());
       Assert.Equal("Porsche", porsche.Brand());
     }
+
+    [Fact]
+    public void BuildsHyperCar()
+    {
+      // Arrange
+      var bugattiBuilder = new BugattiBuilder();
+      var hyperCarBuildDirector = new HyperCarBuildDirector(bugattiBuilder);
+
+      // Act
+      hyperCarBuildDirector.Construct();
+      var bugatti = bugattiBuilder.TheBuiltCar();
+
+      // Assert
+      Assert.NotNull(bugatti);
+      Assert.Equal(2, bugatti.Doors());
+      Assert.Equal("Red", bugatti.Color());
+      Assert.Equal("Bugatti", bugatti.Brand());
+    }
   }
 }
