22c8c5ca69e757377a01090b3a24bd73ab4ce735
ufukkulahli <ufukkulahli@gmail.com>
2020-02-13 23:17:51 +0300

    Continue to 'car builder' example.

--- creational/object/builder/car/HyperCarBuildDirector.cs
+++ creational/object/builder/car/HyperCarBuildDirector.cs
@@ -4,5 +4,10 @@ namespace practicing_design_patterns.creational.@object.builder.car
   {
     private ICarBuilder carBuilder;
     public HyperCarBuildDirector(ICarBuilder carBuilder) => this.carBuilder = carBuilder;
+    public void Construct()
+    {
+      this.carBuilder.Color("Red");
+      this.carBuilder.Doors(2);
+    }
   }
 }
