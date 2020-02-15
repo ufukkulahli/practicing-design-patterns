c9912e97e84f8f1de2e42fb302d8185b0b9a6c50
ufukkulahli <ufukkulahli@gmail.com>
2020-02-15 23:18:28 +0300

    New example named 'connection string' for 'Singleton' pattern.

---
+++ creational/object/singleton/connection-string/ConnectionStringUnitTests.cs
@@ -0,0 +1,18 @@
+using Xunit;
+
+namespace practicing_design_patterns.creational.@object.singleton.connection_string
+{
+  public class ConnectionStringUnitTests
+  {
+    [Fact]
+    public void CreatesOneInstanceOfConnectionString()
+    {
+      // Arrange
+
+      // Act
+
+      // Assert
+      Assert.True(true);
+    }
+  }
+}
