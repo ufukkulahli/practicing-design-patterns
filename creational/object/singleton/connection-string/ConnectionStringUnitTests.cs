e9a4737c196b3385cf741f3c44aacb39a9946c27
ufukkulahli <ufukkulahli@gmail.com>
2020-02-16 23:09:18 +0300

    Continue to 'connection string' example.

--- creational/object/singleton/connection-string/ConnectionStringUnitTests.cs
+++ creational/object/singleton/connection-string/ConnectionStringUnitTests.cs
@@ -8,11 +8,13 @@ namespace practicing_design_patterns.creational.@object.singleton.connection_str
     public void CreatesOneInstanceOfConnectionString()
     {
       // Arrange
+      var instance = ConnectionString.Instance();
+      var value = instance.Value();
 
       // Act
 
       // Assert
-      Assert.True(true);
+      Assert.Equal("database=localhost;user=admin;pass=admin", value);
     }
   }
 }
