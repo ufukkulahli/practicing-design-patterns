59a86daeca838c138f27e505d0aa9918a5cd94fc
ufukkulahli <ufukkulahli@gmail.com>
2020-02-17 23:22:00 +0300

    New example for 'Singleton' pattern.

---
+++ creational/object/singleton/database/DatabaseUnitTests.cs
@@ -0,0 +1,18 @@
+using Xunit;
+
+namespace practicing_design_patterns.creational.@object.singleton.database
+{
+  public class DatabaseUnitTests
+  {
+    [Fact]
+    public void CreatesSingleInstanceOfDatabase()
+    {
+      // Arrange
+
+      // Act
+
+      // Assert
+      Assert.True(true);
+    }
+  }
+}
